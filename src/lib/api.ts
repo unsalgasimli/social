6e9d0c19933edda390cf0b3c19b90716dbc79c82
unsalgasimli <115149752+unsalgasimli@users.noreply.github.com>
2025-08-19 13:06:47 +0400

    api.ts fix v1.5

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_API_BASE,
 });
 
-// attach token automatically
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) config.headers.Authorization = `Bearer ${token}`;
