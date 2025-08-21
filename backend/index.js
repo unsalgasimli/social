2510c40a4a15c0382aea97452fd9d69419c579a8
unsalgasimli <115149752+unsalgasimli@users.noreply.github.com>
2025-08-21 12:07:26 +0400

    backend index.js Port Changed to 0.0.0.0

--- backend/index.js
+++ backend/index.js
@@ -127,4 +127,7 @@ app.get("/api/health", (req, res) => res.json({ status: "OK" }));
 
 // ===== Start Server =====
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`✅ Backend listening on port ${PORT}`);
+});
+
