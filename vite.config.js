77ea6c75786cfbb0f6db4786394524740845e61f
unsalgasimli <115149752+unsalgasimli@users.noreply.github.com>
2025-08-21 12:42:43 +0400

    general fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  base: './',
+  root: 'frontend',      // frontend folder is the root
+  base: './',            // relative paths
   build: {
-    outDir: 'dist',
+    outDir: 'dist',      // will create frontend/dist
     emptyOutDir: true,
   },
 });
